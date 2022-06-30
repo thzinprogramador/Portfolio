01d27c0b4ef9a506917f865e96d46f4b8fb4843e
Thzin Programador <107508243+thzinprogramador@users.noreply.github.com>
2022-06-30 08:33:02 -0300

    Portfolio com login

---
+++ portfolio do THzin/javaScript/scriptMenu.js
@@ -0,0 +1,25 @@
+/**
+	JavaScript da animaçao do Menu
+	Code by Thzin Mine-Programador, 2022
+*/
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+/**------------------------------------Code by Thzin Mine-Programador, 2022------------------------------------------*/
